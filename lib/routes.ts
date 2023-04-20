00a1741edc055457f7e993122fc048825bfde192
code_rick <ricardoanj.14@gmail.com>
2023-04-19 23:17:57 -0300

    rimplementa rota de criação de habito

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,9 +1,43 @@
 import { prisma } from './prisma';
 import { FastifyInstance } from 'fastify';
 
+import { z } from 'zod';
+import dayjs from 'dayjs';
+
 export async function appRoutes(app: FastifyInstance) {
   app.get("/", async () => {
-    return await prisma.habit.findMany();
+    const habits = await prisma.habit.findMany({
+      include: {
+        weekDays: true,
+      },
+    });
+
+    return habits;
+  })
+  
+  app.post("/habits", async (request) => {
+    const createHabitSchema = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+
+    const { title, weekDays } = createHabitSchema.parse(request.body);
+
+    const today = dayjs().startOf("day").toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map(weekDay => {
+            return {
+              week_day: weekDay,
+            }
+          }),
+        },
+      },
+    });
   });
   
 }
